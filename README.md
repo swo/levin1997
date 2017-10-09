83c0993cee2f18dc3b95024f12d2d69b47a3d205
swo <swo@alum.mit.edu>
2017-10-09 13:46:28 -0400

    fixed eqn

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Levin et al. simulation
 
 This is an implementation of the simulations described in Levin *et al*., "The population genetics of antibiotic resistance", *Clinical Infectious Diseases* (1997). The paper is [here](https://academic.oup.com/cid/article/24/Supplement_1/S9/283564/The-Population-Genetics-of-Antibiotic-Resistance).
+
+# Derivation of pg. S10 equations
+
+The new environmental resistant fraction is the result of replacing a fraction $f$ with the average host resistance $\overline{H}$ and enforcing selection in the remainder:
+$$
+\begin{aligned}
+E' &= f \overline{H} + (1-f) \frac{(1-s)E}{(1-E) + (1-s)E} \\
+   &= f \overline{H} + (1-f) \frac{(1-s)E}{1 - sE}
+\end{aligned}
+$$
+Thus, the *change* in resistance is:
+$$
+\begin{aligned}
+\Delta E &= E' - E \\
+ &= f \overline{H} + (1-f) \frac{(1-s)E}{1-sE} - \left[ \frac{1-sE}{1-sE} (1-f) + f \right] E \\
+ &= f (\overline{H} - E) + (1-f) \frac{sE(1-E)}{1-sE},
+\end{aligned}
+$$
+which makes it look like there is a missing term in the equation.
+
+Similarly, I would expect:
+$$
+H_i' = gE + (1-g) \frac{(1-s)H_i}{1 - sH_i}
+$$
--- sim.py
+++ sim.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
-# Simulation described in Levin, et al. "Population genetics of antibiotic resistance",
-# CID 1997.
+# Simulation extending Levin 1997. I added a "fraction zero", so that some
+# hosts never get treated.
 
 import numpy as np
 import sys
 
 class Levin:
-    def __init__(self, s, T, f, g, tg, N, n_generations, H0=1e-9, E0=1e-9, log=None, log_interval=100):
-        self.s = s
-        self.T = T
-        self.f = f
-        self.g = g
-        self.tg = tg
-        self.N = N
-        self.n_generations = n_generations
-        self.E = E0
-        self.H = np.full(N, H0)
+    def __init__(self, s, sr, fc, cac, f, g, tg, N, n_generations, H0=1e-9, E0=1e-9, log=None, log_interval=100):
+        self.s = s # selection coefficient favoring susceptible strains
+        self.sr = sr # selection coefficient favoring resistant strains during treatment
+        self.fc = fc # fraction consumers
+        self.cac = cac # average treatments per year among consumers
+        self.f = f # fraction of environment replaced by host-shed bugs
+        self.g = g # fraction of each host's bugs replaced by environmental bugs
+        self.tg = tg # generation time, in years
+        self.N = N # number of hosts
+        self.n_generations = n_generations # simulation duration
+        self.E = E0 # initial resistance in environment compartment
+        self.H = np.full(N, H0) # initial resistance in all hosts
         self.log = log
         self.log_interval = log_interval
 
-        self.p_treatment = T * tg
+        self.n_consumer = int(self.fc * self.N)
+        self.n_zero = self.N - self.n_consumer
+        self.p_treatment = self.cac * self.tg
+
+        assert 0 <= self.p_treatment <= 1
 
     def run(self):
         if self.log is not None:
@@ -42,28 +48,43 @@ class Levin:
         for i in range(n_generations):
             self.advance()
 
+    @staticmethod
+    def select_fraction(s, f):
+        '''New fraction f when strain with fraction f has fitness 1-s and other has 1'''
+        return (1.0 - s) * f / (1.0 - s * f)
+
     def advance(self):
-        # act as if no hosts got drug
-        self.H = self.H + self.g * (self.E - self.H) - self.s * self.H * (1.0 - self.H) / (1.0 - self.s * self.H)
+        # update E using previous H values
+        new_E = self.f * np.mean(self.H) + (1.0 - self.f) * self.select_fraction(s, self.E)
+
+        # update H with previous E value as if no hosts got drug
+        #self.H = self.H + self.g * (self.E - self.H) - self.s * self.H * (1.0 - self.H) / (1.0 - self.s * self.H)
+        self.H = self.g * self.E + (1 - self.g) * self.select_fraction(self.s, self.H)
 
         # then replace some with treatment
-        treatment = np.random.binomial(1, self.p_treatment, size=self.N).astype(float)
+        nonconsumer_treatment = np.full(self.n_zero, 0)
+        consumer_treatment = np.random.binomial(1, self.p_treatment, size=self.n_consumer)
+        treatment = np.concatenate((nonconsumer_treatment, consumer_treatment))
         self.H = np.fmax(self.H, treatment)
 
-        self.E = self.E + self.f * (np.mean(self.H) - self.E) - self.s * self.E * (1 - self.E) / (1.0 - self.s * self.E)
+        #self.E = self.E + self.f * (np.mean(self.H) - self.E) - self.s * self.E * (1 - self.E) / (1.0 - self.s * self.E)
+        self.E = new_E
 
-
-s = 0.005 # selection coefficient favoring susceptible strains
-T = 2.0 # average treatments per year
 f = 0.05 # fraction of environment replaced by host-shed bugs
 g = 0.005 # fraction of each host's bugs replaced by environmental bugs
 tg = 1/219 # generation time, in years
 N = 10000 # number of hosts
 n_generations = int(5e3)
 
-print('s', 'T', 'host', 'env', sep='\t')
-for s in [0.04, 0.03, 0.02, 0.01, 0.005]:
-    for T in np.arange(0, 2.01, 0.2):
-        host, env = Levin(s, T, f, g, tg, N, n_generations).run()
-        print(s, T, host, env, sep='\t')
-        sys.stdout.flush()
+#ss = [0.04, 0.03, 0.02, 0.01, 0.005]
+ss = [0.04, 0.02, 0.005]
+cacs = np.linspace(0.0, 2.0, 5)
+fnzs = np.linspace(0.0, 1.0, num=5)
+
+print('s', 'fnz', 'cac', 'host', 'env', sep='\t')
+for s in ss:
+    for cac in cacs:
+        for fnz in fnzs:
+            host, env = Levin(s, None, fnz, cac, f, g, tg, N, n_generations).run()
+            print(s, fnz, cac, host, env, sep='\t')
+            sys.stdout.flush()
